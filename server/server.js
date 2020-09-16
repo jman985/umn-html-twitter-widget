581133ee4875c4ca99178c5f18baeee0e5ea921b
Josh Maus <jman985@gmail.com>
2020-09-16 00:29:12 -0500

    added v2 using title tag and LIKE query

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 const widgetRouter = require('./routes/widget.router')
+const widgetTitleRouter = require('./routes/widgetTitle.router')
 
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -10,6 +11,7 @@ app.use(express.static('server/public'));
 
 // ROUTES
 app.use('/api/tweets', widgetRouter)
+app.use('/api/tweets2', widgetTitleRouter)
 
 // Start listening for requests on a specific port
 app.listen(PORT, () => {
